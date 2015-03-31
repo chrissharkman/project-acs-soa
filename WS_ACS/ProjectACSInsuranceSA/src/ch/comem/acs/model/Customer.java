a358607933038248de37ea55c3d6483e4b618505
chrissharkman <ch.sharkman@gmail.com>
2015-03-31 10:43:48 +0200

    Add controller and model folder

---
+++ WS_ACS/ProjectACSInsuranceSA/src/ch/comem/acs/model/Customer.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.comem.acs.model;
+
+/**
+ * This class describes a customer with a his id (customer number) and a last name.
+ * @author christian heimann
+ */
+public class Customer {
+    private int id;
+    private String lastname;
+    
+    
+}
