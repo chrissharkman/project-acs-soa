c778103adc0893983975a19b54f6dd817432f8bc
florentplomb <plombf@gmail.com>
2015-03-31 15:00:22 +0200

    getissue

--- WS_SAC/ProjectSACAdminSA/src/ch/comem/controller/SACController.java
+++ WS_SAC/ProjectSACAdminSA/src/ch/comem/controller/SACController.java
@@ -5,7 +5,10 @@
  */
 package ch.comem.controller;
 
+import ch.comem.acs.model.VehicleType;
 import ch.comem.sac.model.Customer;
+import ch.comem.sac.model.Issue;
+import ch.comem.sac.model.Vehicle;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -25,7 +28,7 @@ public class SACController {
     }
 
     /* CRUD METHODE */
-    public static ArrayList<Customer> getCustomer(String lastname) {
+    public static ArrayList<Customer> getCustomers(String lastname) {
 
         ArrayList<Customer> customers = new ArrayList<>();
 
@@ -36,38 +39,32 @@ public class SACController {
             con = DriverManager.getConnection(prop.getString("dbUrl"), prop.getString("dbUser"), prop.getString("dbPw"));
             Statement statement = con.createStatement();
             ResultSet results = statement.executeQuery("SELECT * FROM customers ");
-
             while (results.next()) {
                 Customer customer = new Customer(
                         results.getString("lastname"));
                 customers.add(customer);
             }
-
         } catch (Exception e) {
             System.out.println(e.getMessage());
             customers = null;
         }
-
         try {
             con.close();
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
-
         return customers;
-
     }
 
-    public static Customer insertCustomer(String lastname) {
+    public static Customer getCustomer(int id) {
 
         Customer customer = null;
         Connection con = null;
-
         try {
             ResourceBundle prop = propertiesLoader();
             con = DriverManager.getConnection(prop.getString("dbUrl"), prop.getString("dbUser"), prop.getString("dbPw"));
             Statement statement = con.createStatement();
-            ResultSet results = statement.executeQuery("INSERT TO customers (lastname) VALUES ('" + lastname + "') ");
+            ResultSet results = statement.executeQuery("SELECT * FROM customers where id =" + id);
             if (results.next()) {
                 customer = new Customer(
                         results.getString("lastname"));
@@ -76,45 +73,205 @@ public class SACController {
             System.out.println(e.getMessage());
             customer = null;
         }
-
         try {
             con.close();
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
-
         return customer;
-
     }
 
-    public static Customer getVehicleType(String lastname) {
+    public static int insertCustomer(String lastname) {
 
-        Customer customer = null;
+        int customerInsert = -1;
         Connection con = null;
-
         try {
             ResourceBundle prop = propertiesLoader();
             con = DriverManager.getConnection(prop.getString("dbUrl"), prop.getString("dbUser"), prop.getString("dbPw"));
             Statement statement = con.createStatement();
-            ResultSet results = statement.executeQuery("SELECT * FROM customers ");
-
-            if (results.next()) {
-                customer = new Customer(
-                        results.getString("lastname"));
+            customerInsert = statement.executeUpdate("INSERT TO customers (lastname) VALUES ('" + lastname + "') ");
+            if (customerInsert == -1) {
+                throw new RuntimeException("Customer insert error");
             }
         } catch (Exception e) {
             System.out.println(e.getMessage());
-            customer = null;
         }
-
         try {
             con.close();
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
+        return customerInsert;
+    }
 
-        return customer;
+    public static VehicleType getVehicleType(String category) {
 
+        VehicleType vehicleType = null;
+        Connection con = null;
+        try {
+            ResourceBundle prop = propertiesLoader();
+            con = DriverManager.getConnection(prop.getString("dbUrl"), prop.getString("dbUser"), prop.getString("dbPw"));
+            Statement statement = con.createStatement();
+            ResultSet results = statement.executeQuery("SELECT * FROM vehiclesType WHERE category = '" + category + "'");
+            if (results.next()) {
+                vehicleType = new VehicleType(
+                        results.getString("categorie"));
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            vehicleType = null;
+        }
+        try {
+            con.close();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return vehicleType;
+    }
+
+    public static int insertVehicleType(String category) {
+
+        int vehicleTyperInsert = -1;
+        Connection con = null;
+        try {
+            ResourceBundle prop = propertiesLoader();
+            con = DriverManager.getConnection(prop.getString("dbUrl"), prop.getString("dbUser"), prop.getString("dbPw"));
+            Statement statement = con.createStatement();
+            vehicleTyperInsert = statement.executeUpdate("INSERT TO VehicleType (categorie) VALUES ('" + category + "') ");
+            if (vehicleTyperInsert == -1) {
+                throw new RuntimeException("Customer insert error");
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        try {
+            con.close();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return vehicleTyperInsert;
+    }
+
+    public static ArrayList<Vehicle> getVehicles() {
+        ArrayList<Vehicle> vehicles = new ArrayList<>();
+        Connection con = null;
+        try {
+            ResourceBundle prop = propertiesLoader();
+            con = DriverManager.getConnection(prop.getString("dbUrl"), prop.getString("dbUser"), prop.getString("dbPw"));
+            Statement statement = con.createStatement();
+            ResultSet results = statement.executeQuery("SELECT * FROM vehicles ");
+            while (results.next()) {
+                Vehicle vehicle = new Vehicle(
+                        results.getString("identificationNumber"),
+                        results.getString("moedel"),
+                        results.getString("typeCategory")
+                );
+                vehicles.add(vehicle);
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            vehicles = null;
+        }
+        try {
+            con.close();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return vehicles;
+    }
+    
+        public static Vehicle getVehicle(String identificationNumber ) {
+        Vehicle vehicle = null;
+        Connection con = null;
+        try {
+            ResourceBundle prop = propertiesLoader();
+            con = DriverManager.getConnection(prop.getString("dbUrl"), prop.getString("dbUser"), prop.getString("dbPw"));
+            Statement statement = con.createStatement();
+            ResultSet results = statement.executeQuery("SELECT * FROM vehicles WHERE identificationNumber = '"+identificationNumber+"'");
+            if (results.next()) {
+               vehicle = new Vehicle(
+                        results.getString("identificationNumber"),
+                        results.getString("moedel"),
+                        results.getString("typeCategory")
+                );          
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            vehicle = null;
+        }
+        try {
+            con.close();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return vehicle;
+    }
+
+    public static int insertVehicle(Vehicle vehicule) {
+
+        int vehicleInsert = -1;
+        Connection con = null;
+        try {
+            ResourceBundle prop = propertiesLoader();
+            con = DriverManager.getConnection(prop.getString("dbUrl"), prop.getString("dbUser"), prop.getString("dbPw"));
+            Statement statement = con.createStatement();
+            vehicleInsert = statement.executeUpdate("INSERT TO VehicleType (identificationNumber,typeCategory,model) VALUES "
+                    + "('" + vehicule.getIdentificationNumber() + "', "
+                    + "'" + vehicule.getTypeCategory() + "',"
+                    + "'" + vehicule.getTypeCategory() + "'");
+            if (vehicleInsert == -1) {
+                throw new RuntimeException("vehicle insert error");
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        try {
+            con.close();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return vehicleInsert;
+    }
+
+    /**
+     * public Issue(int id, String numberPlate, Customer customer, Vehicle
+     * vehicle, String status, String comment, Date createdDate, Date handOut,
+     * Date closeIssueDate) {
+     *
+     * @return
+     */
+    public static ArrayList<Issue> getIssue() {
+        ArrayList<Issue> issues = new ArrayList<>();
+        Connection con = null;
+        try {
+            ResourceBundle prop = propertiesLoader();
+            con = DriverManager.getConnection(prop.getString("dbUrl"), prop.getString("dbUser"), prop.getString("dbPw"));
+            Statement statement = con.createStatement();
+            ResultSet results = statement.executeQuery("SELECT * FROM issues ");
+            while (results.next()) {
+                Issue issue = new Issue(
+                        results.getInt("id"),
+                        results.getString("numberPlate"),
+                        getCustomer(results.getInt("customerId")),
+                        getVehicle(results.getString("identificationNumber")),
+                        results.getString("status"),
+                        results.getString("comment"),
+                        results.getDate("createdDate"),
+                        results.getDate("handOut"),
+                        results.getDate("closeIssueDate")                                                      
+                );
+                issues.add(issue);
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            issues = null;
+        }
+        try {
+            con.close();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return issues;
     }
 
     /* PRIVATE METHODE */
